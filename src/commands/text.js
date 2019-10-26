525bd562979a46977fab177228e53e6d327a468e
jkieberking <jkieberking@gmail.com>
2019-10-26 02:16:59 -0400

    root path

--- src/commands/text.js
+++ src/commands/text.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const appRoot = require('app-root-path');
 
 function checkTextCommands(message, command, args) {
     if (command === 'TeXit') {
@@ -14,8 +15,9 @@ function checkTextCommands(message, command, args) {
     }
 
     if (command === 'dot') {
-        fs.readFile('../../storage/s-dot.txt', 'utf8', function(err, data) {
+        fs.readFile(appRoot + '/storage/s-dot.txt', 'utf8', function(err, data) {
             if (err) {
+                console.log('cant read file');
                 return;
             }
             message.channel.send('data');
