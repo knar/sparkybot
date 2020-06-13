31d4368f1161c699fbef1cecd72fd62835142522
jkieberking <jkieberking@gmail.com>
2020-06-13 17:25:05 -0400

    limit rolecount

--- src/commands/roles.js
+++ src/commands/roles.js
@@ -1,5 +1,9 @@
 const helper = require('../lib/helper');
 
+const BOT_ROLES = [
+    'voicemaster',
+]
+
 function checkRoleCount(message, command, args) {
     if (command == 'rolecount') {
         const guild = message.guild;
@@ -10,7 +14,10 @@ function checkRoleCount(message, command, args) {
 
         if (roles !== null) {
             messageArray = [];
+            let numRoles = 0;
             for (const [roleId, role] of roles) {
+                if (BOT_ROLES.includes(role.name.toLowerCase())) { continue; }
+                if (numRoles++ > 1) { break; }
                 const memberCount = guild.roles.get(roleId).members.size;
                 let membersMessage = (1 === memberCount) ? 'member' : 'members';
                 messageArray.push(`**${role.name}** currently has **${memberCount}** **${membersMessage}**`);
